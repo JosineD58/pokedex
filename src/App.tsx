cf043857b42c7f6ad8b0494f18c28c5f2aad1f7d
JosineD58 <106514121+JosineD58@users.noreply.github.com>
2022-06-23 10:08:40 +0200

    HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { Routes, Route } from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return <div className="App"></div>;
-  <Routes>
-    <Route path="/discover" element={<DiscoverPage />} />
-    <Route path="/about" element={<AboutPage />} />
-    <Route path="/" element={<HomePage />} />
-  </Routes>;
+  return (
+    <div className="App">
+      <NavBar>
+        <NavLink></NavLink>
+      </NavBar>
+      <Routes>
+        <Route path="/discover" element={<DiscoverPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+      ;
+    </div>
+  );
 }
 
 export default App;
