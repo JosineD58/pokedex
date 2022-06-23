7c3af85adc0bbdabd59c57d98b23808f46334fbc
JosineD58 <106514121+JosineD58@users.noreply.github.com>
2022-06-23 15:28:33 +0200

    homeImage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,11 @@ const HomePage = () => {
   return (
     <div>
       <h1 className="titleHome">Home</h1>
+      <img
+        className="homeImage"
+        src="https://www.kindpng.com/picc/m/57-575100_pokemon-png-transparent-png.png"
+        alt="pokemon"
+      />
     </div>
   );
 };
