30d978afc407b5d019fba2a5e96152fdb07b9532
JosineD58 <106514121+JosineD58@users.noreply.github.com>
2022-06-23 10:16:40 +0200

    Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
 import DiscoverPage from "./pages/DiscoverPage";
@@ -8,15 +8,17 @@ import DiscoverPage from "./pages/DiscoverPage";
 function App() {
   return (
     <div className="App">
-      <NavBar>
-        <NavLink></NavLink>
-      </NavBar>
+      <div className="nav">
+        <h1>Navbar</h1>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/discover">Discover</NavLink>
+      </div>
       <Routes>
         <Route path="/discover" element={<DiscoverPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/" element={<HomePage />} />
       </Routes>
-      ;
     </div>
   );
 }
