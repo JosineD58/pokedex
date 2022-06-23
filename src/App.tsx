32f76824c5d386b0069e496ad9c4ab9cc449c97b
JosineD58 <106514121+JosineD58@users.noreply.github.com>
2022-06-23 10:01:34 +0200

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return <div className="App"></div>;
+  <Routes>
+    <Route path="/discover" element={<DiscoverPage />} />
+    <Route path="/about" element={<AboutPage />} />
+    <Route path="/" element={<HomePage />} />
+  </Routes>;
 }
 
 export default App;
